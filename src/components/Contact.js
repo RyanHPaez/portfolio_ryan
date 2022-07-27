8b952adbc2540c48195420df09610d630897564d
RyanHPaez <solosnail@gmail.com>
2022-07-27 11:03:36 -0700

    css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-sm-6">
+    <div className="contact" >
+      <div class="container" >
+        <div class="row align-items-center my-5" >
+          <div class="col-sm-12">
             <img
               class="rounded-circle  row align-items-center my-5"
               src="https://media-exp1.licdn.com/dms/image/C4E03AQGV7LQ_7Q-aTw/profile-displayphoto-shrink_200_200/0/1591653389941?e=1664409600&v=beta&t=2-YMs4gXbovh9xjD3JDmEMOisp1yCsQDQZ9gccZmFEY"
@@ -15,7 +15,7 @@ function Contact() {
               position= "fixed"
             />
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-12">
             <h1 class="font-weight-light">Contact</h1>
             
      <p>Ryan Paez:</p>
