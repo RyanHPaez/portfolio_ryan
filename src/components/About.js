94814ed88b98117e0df53739d6914b0c40c5a5a2
RyanHPaez <solosnail@gmail.com>
2022-07-27 01:15:39 -0700

    bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div className="about">
       <div class="container">
         <div class="row align-items-center my-4">
-        <div class="col-md-7 float-right ">
+        <div class="col-md-7 pl-4">
             
             <h5>I have a GitHub with over 100 Repositories</h5>
             <a href={"https://github.com/RyanHPaez?tab=repositories"}>
@@ -13,14 +13,15 @@ function About() {
             </a>
           </div>
           
-          <div class="col-md-5">
+          <div class="col-md-5 shadow-lg p-1 mb-1 bg-white">
             <img
               class="rounded-circle col-md-12 "
               src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/133515411_2901941880029508_174536494781446050_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=1My55PKtyS0AX-rUGPi&tn=TxyqeLy8NatzAtC5&_nc_ht=scontent-lax3-1.xx&oh=00_AT8Tdc0LexMb4TjUvH6vYqp_-HmHXmJDtcp7iDq7gnsP6A&oe=63076FB4"
               alt="Daddy and V"
             />
-          </div></div>
-          <div class="col-md-12">
+          </div>
+          </div>
+          <div class="col-md-12 my-5">
             <h1 class="font-weight-light">My Skills</h1>
             <p>Languages & Tools:</p>
             <p>JavaScript (React & Redux), TypeScript, Node.js, HTML & CSS, SASS, GitHub, GitBash, Devtools, Python, AWS, Heroku, Django, Cybersecurity, Databases: MongoDB, PostgreSQL, SQL</p>
