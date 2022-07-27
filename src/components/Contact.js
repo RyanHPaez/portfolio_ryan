bd70f7b2dd72e3472caf6fe885926a613f481960
RyanHPaez <solosnail@gmail.com>
2022-07-26 23:23:58 -0700

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,14 @@ function Contact() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-5">
-            {/* <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            /> */}
+            <img
+              class="rounded-circle float-right"
+              src="https://media-exp1.licdn.com/dms/image/C4E03AQGV7LQ_7Q-aTw/profile-displayphoto-shrink_200_200/0/1591653389941?e=1664409600&v=beta&t=2-YMs4gXbovh9xjD3JDmEMOisp1yCsQDQZ9gccZmFEY"
+              alt="image"
+              width='240px'
+              height='200px'
+              position= "fixed"
+            />
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Contact</h1>
