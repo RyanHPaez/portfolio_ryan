f1e6486dbb72b7b329beb43777243363f81326f4
RyanHPaez <solosnail@gmail.com>
2022-07-27 12:17:24 -0700

    css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-function Footer() {
-  return (
-    <div className="footer">
-      <footer class="py-2 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-             Ryan Paez 2022
-          </p>
-        </div>
-      </footer>
-    </div>
-  );
-}
+// function Footer() {
+//   return (
+//     <div className="footer">
+//       <footer class="py-2 bg-dark fixed-bottom">
+//         <div class="container">
+//           <p class="m-0 text-center text-white">
+//              Ryan Paez 2022
+//           </p>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// }
 
-export default Footer;
+// export default Footer;
