55ad9b5fc23de7ffb38aa2435c6345eca090aa80
RyanHPaez <solosnail@gmail.com>
2022-07-26 20:22:01 -0700

    updates of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
           <div class="col-lg-5">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.18169-9/374416_364683393644397_73091407_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=tGoIL_RmOdwAX-P0hCL&_nc_ht=scontent-lax3-1.xx&oh=00_AT-3JW3qv_XbGwI5LOQFl8aWgBFvWcbVP18bMYfDf6lfpA&oe=6304F308"
+              src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/44972081_1824995524279836_3745263644489809920_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=naGqf5cD4uEAX-4sNEH&_nc_ht=scontent-lax3-1.xx&oh=00_AT-DjqG-bos7UTDC6VkhqEUrOn7-ERumGLGgyGHopn6s2Q&oe=63058C3B"
               alt="me"
             />
           </div>
