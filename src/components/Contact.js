77d28a5a026b4aa29ce8e7a1110f9df05ce7bc3e
RyanHPaez <solosnail@gmail.com>
2022-07-28 11:55:16 -0700

    css root

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,25 +2,24 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="contact" >
-      <div class="container" >
-        <div class="row align-items-center my-5" >
+    <div className="contact">
+      <div class="container">
+        <div class="row align-items-center my-5">
           <div class="col-sm-12 ">
             <img
               class="rounded-circle  "
               src="https://media-exp1.licdn.com/dms/image/C4E03AQGV7LQ_7Q-aTw/profile-displayphoto-shrink_200_200/0/1591653389941?e=1664409600&v=beta&t=2-YMs4gXbovh9xjD3JDmEMOisp1yCsQDQZ9gccZmFEY"
               alt="me"
-              
-              
             />
           </div>
           <div class="col-sm-12">
             <h1 class="font-weight-light">Contact</h1>
-            
-     <p>Ryan Paez:</p>
-     
-     <a href={"https://www.linkedin.com/in/ryan-paez/"}>Click here to go to my LinkedIn</a>
-  
+
+            <p>Ryan Paez:</p>
+
+            <a href={"https://www.linkedin.com/in/ryan-paez/"}>
+              Click here to go to my LinkedIn
+            </a>
           </div>
         </div>
       </div>
