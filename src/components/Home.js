4de6a6711fb4fc0f063d8d1f016487b2596e06f5
RyanHPaez <solosnail@gmail.com>
2022-07-28 17:10:19 -0700

    about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class='col-md-6'>
+          <div class="col-md-6">
             <img
               class="img-fluid"
               src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/44972081_1824995524279836_3745263644489809920_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=naGqf5cD4uEAX-4sNEH&_nc_ht=scontent-lax3-1.xx&oh=00_AT-DjqG-bos7UTDC6VkhqEUrOn7-ERumGLGgyGHopn6s2Q&oe=63058C3B"
@@ -15,23 +15,27 @@ function Home() {
           <div class="col-sm-5 ml-5 my-5">
             <h1 class="font-weight-light ">Hi, My name is Ryan</h1>
             <p>
-             Ready for my next journey in life to start as a Full Stack Developer!
+              Ready for my next journey in life to start as a Full Stack
+              Developer!
             </p>
           </div>
           <div class="col-sm-12 my-4">
-          <h1 class="font-weight-light">About Me</h1>
-          <p>
-            I am father of two perfect children and a husband to the best wife
-            in the world. Currently working as a Aircraft Mechanic at Western
-            Jet Aviation in Van Nuys, CA. I love staying busy, focused and
-            challenged and am a excellent team player with a great personality,
-            with amazing drive and am very persistent. When I found coding I
-            learned that it fits into what I am a natural at. Never giving up!
-            Constantly being challenged is something I love. I see my future as
-            being happy and thankful for always wanting to do better.
-          </p>
-          <p>- Ryan Paez</p>
-        </div>
+            <h1 class="font-weight-light">About Me</h1>
+            <p>
+              I am a father of two perfect children and a husband to the best
+              wife in the world. Currently working as a Aircraft Mechanic at
+              Western Jet Aviation in Van Nuys, CA. Love staying busy, focused
+              and challenged, being very persistent with amazing drive. An
+              excellent team player with a great personality, easy to talk to
+              and also known to be a genuine guy. Coding found me and I learned
+              that it fits into what I am a natural at. Never giving up! My
+              present and future is blessed with happiness that comes from
+              having a positive attitude. Proud of all accomplishments achieved
+              and future accomplishments that will be achieved. Wanting to
+              mature will always be a thing for me.
+            </p>
+            <p>- Ryan Paez</p>
+          </div>
         </div>
       </div>
     </div>
