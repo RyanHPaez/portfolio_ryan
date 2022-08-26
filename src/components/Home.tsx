534f530d1716f024a066e22e5c10c20f7522e3a2
RyanHPaez <solosnail@gmail.com>
2022-08-26 14:43:15 -0700

    updates

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,11 +6,11 @@ function Home() {
       <div className="container">
         <div className="row align-items-center my-5">
           <div className="col-md-6">
-            <img
-              className="img-fluid"
-              src="MeDelta.jpg"
-              alt="me"
-            />
+          <img
+                className="img-fluid "
+                src="https://olyortho.com/wp-content/uploads/2020/09/AdobeStock_124565984-1-scaled.jpeg"
+                alt="hills"
+              />
           </div>
           <div className="col-md-6  my-5 text-center text-justify">
             <h1 className="Hi font-weight-light ">Hi, My name is Ryan</h1>
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,20 +2,52 @@ import React from "react";
 
 function Skill() {
   return (
-    <div className="container">
+    
+        <div className="resume container bg-dark my-5">
+          <div className="row">
+            <div className="col-sm-4 text-center"> </div>
+            <div className="col-sm-4 my-3  text-center">
+            <div className="container">
       <div className="row">
         <div className="col-sm-12 my-4 text-center">
           <h1 className="font-weight-light  ">My Skills</h1>
           <p>Languages & Tools:</p>
-          <p>
-            JavaScript (React & Redux), TypeScript, Node.js, HTML & CSS, SASS,
-            GitHub, GitBash, Devtools, Python, AWS, Heroku, Django,
-            Cybersecurity, 
-          </p>
+          
+          JavaScript,
+          
+          React, 
+
+          Redux, 
+          
+          Node.js, 
+         
+          HTML, 
+          
+          CSS, 
+          
+          SASS,
+          
+          GitHub, 
+            
+          GitBash, 
+            
+          Devtools, 
+            
+          Python, 
+
+          TypeScript, 
+            
+          AWS, 
+            
+          Heroku, 
+            
+          Django,
+            
+          Cybersecurity
           <p>
           Databases: MongoDB, PostgreSQL, SQL
           </p>
-          <div className="col-sm-12  my-4 text-center">
+          {/* <div className="col-sm-12  my-4 text-center">
           
           <iframe
             src="https://hikingforstars.herokuapp.com/">
@@ -25,7 +57,7 @@ function Skill() {
             src="https://gamestorejar.herokuapp.com/">
             </iframe>
             
-            </div>
+            </div> */}
         </div>
         <div className="col-sm-12  my-4 text-center">
           <h5>I have a GitHub with over 100 Repositories</h5>
@@ -34,15 +66,7 @@ function Skill() {
                                 </a>
                              
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-4 text-center"> </div>
-            <div className="col-sm-4 my-3  text-center">
-              <img
-                className="img-fluid"
-                src="daddyValerie.jpg"
-                alt="Daddy and V"
-              />
+              
               <div className="col-sm-4 my-3  text-center"></div>
             </div>
           </div>
