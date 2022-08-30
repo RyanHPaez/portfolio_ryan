b23608602dd3f0ff0bddcd0c512c40c5e3c7f267
RyanHPaez <solosnail@gmail.com>
2022-08-29 19:18:17 -0700

    personal  image added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,14 +19,11 @@ function Home() {
       }
     }}
     >      
-      <div className="container">      
+      <div className="container ">      
         <div className="row align-items-center  my-5">
-          <div className="col-md-6">
-          <img
-                className="img-fluid "
-                src="https://olyortho.com/wp-content/uploads/2020/09/AdobeStock_124565984-1-scaled.jpeg"
-                alt="hills"
-              />
+          <div className="col-md-6 my-5 ">
+          <img src={require('../images/dv.jpg'   ) } id='dv' ></img>
+          
           </div>
           <div className="col-md-6  my-5 text-center text-justify">
             <h1 className="Hi font-weight-light ">Hi, My name is Ryan</h1>
