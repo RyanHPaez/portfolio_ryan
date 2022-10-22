9ed368a1cd717976273fd69e790b202ba4e9abbe
ryanhpaez <solosnail@gmail.com>
2022-10-21 18:15:55 -0700

    got rid of all small errors

--- src/components/AminmationRoutes.tsx
+++ src/components/AminmationRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import {  Home, Skill, Contact } from "../components";
 import {AnimatePresence} from "framer-motion"
 
